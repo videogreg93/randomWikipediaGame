4b6aebe7c6f0202efa7ac075afb9ead9931421cf
Gregory <gregorykarlfournier@gmail.com>
2016-10-19 18:37:01 -0400

    Its now multiple choice

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
+  
+
 $('#startButton').click(function() {
     $('#article').html(getArticle());
+  
 });
+
+
 
 $('#submitAnswer').click(function() {
     var answer = $('#answer').val();
@@ -43,12 +48,122 @@ function getArticle() {
             $('#mainContent').css("display", "inherit");
             $('#startButton').css("display", "none");
             localStorage.setItem("answer", v.title.toLowerCase());
-          return temp;
+            setTimeout(getAnswers(),100);
+            
+          //return temp;
         });
            
         });
     });
     
 });
-return temp;
+//return temp;
+}
+
+function getAnswers() {
+    
+var allAnswers = new Array();
+    allAnswers[0] = localStorage.getItem("answer");
+    var done = 0;
+            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&explaintext&exintro=&format=json&callback=?", function (data) {
+            $.each(data.query.pages, function(k, v) {
+            $.getJSON('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&exintro=&pageids='+v.pageid+'&inprop=url&format=json&callback=?', function(url) {
+                $.each(data.query.pages, function(k, v) {
+                    
+                   allAnswers[1] = v.title.toLowerCase();
+                   done++;
+                });
+               
+            });
+            });
+    
+        });
+            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&explaintext&exintro=&format=json&callback=?", function (data) {
+            $.each(data.query.pages, function(k, v) {
+            $.getJSON('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&exintro=&pageids='+v.pageid+'&inprop=url&format=json&callback=?', function(url) {
+                $.each(data.query.pages, function(k, v) {
+                    
+                   allAnswers[2] = v.title.toLowerCase();
+                   done++;
+                });
+               
+            });
+            });
+    
+        });
+            $.getJSON("https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&explaintext&exintro=&format=json&callback=?", function (data) {
+            $.each(data.query.pages, function(k, v) {
+            $.getJSON('https://en.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&exintro=&pageids='+v.pageid+'&inprop=url&format=json&callback=?', function(url) {
+                $.each(data.query.pages, function(k, v) {
+                    
+                   allAnswers[3] = v.title.toLowerCase();
+                   done++;
+                });
+               
+            });
+            });
+    
+        });
+
+
+    
+    setTimeout(function() {
+                allAnswers = shuffle(allAnswers);
+
+              
+    
+    $('#answers').append("<li class = 'posAnswer'>" + allAnswers[0] + "</li>");
+    $('#answers').append("<li class = 'posAnswer'>" + allAnswers[1] + "</li>");
+    $('#answers').append("<li class = 'posAnswer'>" + allAnswers[2] + "</li>");
+    $('#answers').append("<li class = 'posAnswer'>" + allAnswers[3] + "</li>");
+    
+    $('.posAnswer').click(function() {
+    answer = $(this).html();
+        if (answer == localStorage.getItem("answer")) {
+        $('#answers').html(""); 
+        alert("You win. wow.");
+        var wins = parseInt($("#win").html().replace(/\D/g,''));
+        wins += 1;
+        if (wins == 1) {
+            $("#win").html("You have won " + wins + " time");
+        }
+        else {
+            $("#win").html("You have won " + wins + " times");
+        }
+
+    }
+    else {
+        alert("Looking for " + localStorage.getItem("answer") + " not " + answer );
+        $('#answers').html(""); 
+    }
+    
+            $('#mainContent').css('display', 'none');
+        $('#startButton').css('display', 'inherit');
+ 
+  
+});
+        
+    },300);
+
+  
+
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
